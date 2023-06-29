8ee10e1d71eaba18f601eab305395345991c7c93
nthapa0000 <21bma029@nith.ac.in>
2023-06-29 18:45:37 +0530

    Gitinore Concept

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+// event loop allows Node.js to perform non-blocking I/O operations-despite the fact that JavaScript is single-threaded-by offloading operations to the system kernel whenever possible.
+
+// npm run dev to run the application
+
+// npm uninstall <packageName> to uninstall the package
+// Nuclear approach will uninstall whole node_modules 
+// delete the lock-json file and then delete the depencies from package.json file then do the npm install
+// now we will download nodemon globally so that we can use it in any project
+// sudo npm install -g nodemon in the terminal
+
+// Earlier we had to use the create-react-app globally but now we can use it by npx command
+// npx create-react-app <appName>
+
+
+
+// comments are not allowed in the package.json
+// dev dependency  using nodemon
+// we wont use nodemon in production to restart application , we will use like pm2 to restart application
+
 // in node modules folder all the dependencies are stored
 // if we dont have that folder npm create that folder and store all the dependencies in that folder
 //  if we download bootstrap dependecies then if it require some other dependencies it will also download that dependencies
@@ -31,4 +50,5 @@ const items = [1,[2,[3,[4]]]]
 const newItems = _.flattenDeep(items);
 // we have access to it because we have imported it
 console.log(newItems);
+console.log('hello FRANDS');
 // this methods is in lodash package
